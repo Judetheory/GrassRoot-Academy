a23d71f508423236c858aeba182595627180ebda
Jude Tochukwu Ogbozor <judtheory@gmail.com>
2023-04-30 23:56:37 +0100

    Symbols Variable Declaration

--- rock-papper-scissors/app.js
+++ rock-papper-scissors/app.js
@@ -1,1 +1,9 @@
-console.log("hello world!")
+const humanScore = 0;
+const computerScore = 0;
+const humanScore_span = document.getElementById("hum_score");
+const computerScore_span = document.getElementById("comp_score");
+const scoreBoard_div = document.querySelector(".score_board");
+const Result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
