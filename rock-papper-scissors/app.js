4fbcb82177182e52830f00e8e5ad33641b88fe11
Jude Tochukwu Ogbozor <judtheory@gmail.com>
2023-05-03 22:42:55 +0100

    Computer Choice Function

--- rock-papper-scissors/app.js
+++ rock-papper-scissors/app.js
@@ -8,6 +8,15 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
+function GetComputerChoices() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber]; 
+
+}
+
+console.log(GetComputerChoices());
+
 rock_div.addEventListener("click", function()
 {console.log("Hey! you clicked rock")})
 
